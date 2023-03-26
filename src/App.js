f5db7874a5ab907e2d5eda5008cfa8e4ce08a000
Maaz <maaz97965@gmail.com>
2023-03-27 01:13:29 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles/app.scss";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
